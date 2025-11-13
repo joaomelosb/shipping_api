71db5c9cbec753053db825a9fe87d1f7d5c96f0f
John <jjvictor385@gmail.com>
2025-11-12 22:15:41 -0300

    first commit

---
+++ services/shipping/app.py
@@ -0,0 +1,107 @@
+import json
+import requests
+from os import environ
+from pydantic import BaseModel
+from fastapi import FastAPI, Header, HTTPException
+
+BASE_URL = 'https://www.eleganceoutletbsb.com.br'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
+CHECKOUT_API_URL = 'https://checkout-api.ms.tiendanube.com/checkout/v3/new-shipping-options'
+
+app = FastAPI()
+
+class Variant(BaseModel):
+	id: str
+	name: str
+	product_id: str
+	quantity: int
+
+class Request(BaseModel):
+	zipcode: str
+	variants: list[Variant]
+
+def add_to_cart(session: requests.Session, variant: Variant) -> requests.Response:
+	return session.post(
+		f'{BASE_URL}/comprar/',
+		data = {
+			'add_to_cart': variant.product_id,
+			'variant_id': variant.id,
+			'variant[0]': variant.name,
+			'quantity': variant.quantity,
+			'zipcode': '',
+			'add_to_cart_enhanced': '1'
+		},
+		headers = {
+			'X-Requested-With': 'XMLHttpRequest'
+		}
+	)
+
+def go_to_checkout(session: requests.Session, products: list[dict]) -> str:
+	res = session.post(
+		f'{BASE_URL}/comprar/',
+		data = {
+			**{f'quantity[{product.get("id")}]': product.get("quantity") for product in products},
+			'go_to_checkout': 'Iniciar Compra'
+		}
+	)
+
+	access_token = None
+
+	for key, value in res.cookies.items():
+		if key.startswith('access_token_'):
+			access_token = value
+			break
+
+	return access_token
+
+def get_shipping_data(
+	session: requests.Session,
+	zipcode: str,
+	cart: dict,
+	access_token: str,
+	keys: list[str] = []
+) -> list[dict]:
+	res = session.get(
+		CHECKOUT_API_URL,
+		params = {
+			'cartId': cart.get('id'),
+			'zipcode': zipcode,
+			'country': 'BR',
+			'keepCartAddress': 'false',
+			'city': '',
+			'orderId': cart.get('id')
+		},
+		headers = {
+			'Authorization': f'Bearer {access_token}'
+		}
+	)
+	shipping_data = res.json().get('shipping_options', [])
+
+	if len(keys):
+		shipping_data = list({key: option.get(key) for key in keys} for option in shipping_data)
+
+	return shipping_data
+
+@app.post('/shipping')
+def shipping(data: Request, token: str = Header(...)) -> list:
+	env_token = environ.get('TOKEN')
+
+	if env_token is None or env_token != token:
+		raise HTTPException(403, 'Invalid access token')
+
+	session = requests.Session()
+	session.headers['User-Agent'] = USER_AGENT
+
+	for variant in data.variants:
+		res = add_to_cart(session, variant)
+
+	cart = res.json().get('cart', {})
+	products = cart.get('products', [])
+
+	print(f'cart id: {cart.get("id")}')
+
+	checkout_token = go_to_checkout(session, products)
+
+	print(f'checkout token: {checkout_token}')
+
+	return get_shipping_data(session, data.zipcode, cart, checkout_token)
